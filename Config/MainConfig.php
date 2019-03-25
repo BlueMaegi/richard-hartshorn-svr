8a31dd62e5491f64f2bbd7aabe674c0d648a48e6
Tami Hartshorn <thartshorn42@gmail.com>
2019-03-24 21:32:57 -0400

    Fix server config for linux

--- Config/MainConfig.php
+++ Config/MainConfig.php
@@ -1,11 +1,11 @@
 <?php
 define("WEBROOT", "http://".$_SERVER['HTTP_HOST'].'/');
 define("DOCROOT", $_SERVER['DOCUMENT_ROOT'].'/');
-define("SERVROOT", '/Applications/MAMP/richard-api/');
+define("SERVROOT", '/var/www/richard-hartshorn-svr/');
 
 define("DB_NAME", 'RichardWeb');
 define("DB_UNAME", 'server_app');
-define("DB_PWD", 'password');
+define("DB_PWD", 'lannister5');
 define("DB_SERVER", 'localhost:8889');
 
 ?>
